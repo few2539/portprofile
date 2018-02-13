831b1feb262cca747e01a6961d5f72005d10bd56
few2539 <few_phuridate@hotmail.com>
2018-02-13 15:34:54 +0700

    Add files via upload

---
+++ css/ready.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>.:: Few ::.</title>
+    <link rel="shortcut icon" type="image/png" href="image/logogo.png"/>
+     <!-- BS HERE -->
+     
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <!-- css HERE -->
+    <link href="css/loading.css" rel = "stylesheet">
+    <link href="css/sqloading.css" rel = "stylesheet">
+    <link href="css/dotload.css" rel = "stylesheet">
+    <link href="css/go.css" rel = "stylesheet">
+    <link href="css/bigloader.css" rel = "stylesheet">
+    <link href="css/header.css" rel = "stylesheet">
+    <link href="css/navbar.css" rel = "stylesheet">
+
+    <!-- font HERE -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300" rel="stylesheet"><!-- font HERE -->
+
+
+    <!-- prepare loading HERE -->
+   
+<style>
+  html {
+   
+    overflow: hidden;
+  }
+
+</style>
+</head>
+
+<body>
+
+<!-- First Loader HERE-->
+
+<section>
+<div class="container" >
+    <div class="row text-center">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 load-sad">
+          <div class="wrapper">
+            <div id="loader-wrapper">
+            
+             <div class="preloader"style="border-style:solid; border-color: #f0f0f0; padding:2em; border-width: 5px 5px 5px 20px;">      
+                <div  class="sk-cube-grid" >
+                            <div class="sk-cube sk-cube1"></div>
+                            <div class="sk-cube sk-cube2"></div>
+                            <div class="sk-cube sk-cube3"></div>
+                            <div class="sk-cube sk-cube4"></div>
+                            <div class="sk-cube sk-cube5"></div>
+                            <div class="sk-cube sk-cube6"></div>
+                            <div class="sk-cube sk-cube7"></div>
+                            <div class="sk-cube sk-cube8"></div>
+                            <div class="sk-cube sk-cube9"></div>
+             </div>
+             <div class="loader" style="font-family: 'Raleway', sans-serif; ">
+                        <span1>{</span1>Few's Portfolio<span1>}</span1></br>
+                </div>
+                <p style ="font-family: 'Raleway', sans-serif; " class="dotload">Loading </p>
+</div>
+                    <div class="loader-section section-left"></div>
+                    <div class="loader-section section-right"></div>
+
+                </div>  
+             </div>
+
+    </div>
+</div>
+</section>
+<!--navbar-->
+
+
+
+<!--AFTER LOAD-->
+
+
+
+
+<header class="masthead text-center text-white d-flex" >
+
+
+    <div class="container my-auto">
+       <div class="row">
+           <div class="col-lg-10 col-sm-12 col-md-12 col-xs-12 mx-auto">
+                    
+
+                             <!-- center text FEW-->
+                             <div class="container text-center e1">
+                                    <div class="row">
+                                      <div class="col-lg-12 col-sm-12 col-md-12 col-xs-12 mx-auto" style="padding-bottom: 20px;">
+
+                                          <h1f  class="underline" style="font-family: 'Raleway', sans-serif; "><a href="info.html" style="text-decoration: none; color:#fff; border-style: solid; border-width: 5px; padding: 0.5rem; background-color:#00e18a; border-radius: 10px;">open me</a></h1f >
+                           
+                                     </div>
+                                    </div>
+                                </div>
+                              <!-- center text FEW-->   
+                          <hr>
+          </div>
+      
+         <!-- center text king-->
+          <div class="col-lg-8 mx-auto" id="king" style="font-family: 'Raleway', sans-serif; font-weight: 700; ">
+                      <div class='textnaja'>
+                      <div class='contentstudy'></div>
+                      <div class='dash'></div>
+                      
+                  </div> 
+         
+		            </div>
+                </div>
+              </div>
+            
+             
+          
+        </div>
+    </div>
+   
+   
+</header>
+<div class="cd-cover-layer"></div>
+<div class="cd-loading-bar"></div>
+    <!-- GOGOGOGOGOGOGOGO-->
+
+  
+ <!-- SCRIPT HERE -->
+
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/textfade.js"></script>
+<script src="js/go.js"></script>
+
+
+
+ <!-- SCRIPT HEaRE -->
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>.:: Few ::.</title>
+    <link rel="shortcut icon" type="image/png" href="image/logogo.png"/>
+     <!-- BS HERE -->
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- css HERE -->
+    <link href="css/loading.css" rel = "stylesheet">
+    <link href="css/sqloading.css" rel = "stylesheet">
+    <link href="css/dotload.css" rel = "stylesheet">
+    <link href="css/bigloader.css" rel = "stylesheet">
+    <link href="css/header.css" rel = "stylesheet">
+    <link href="css/navbar.css" rel = "stylesheet">
+    <link rel="stylesheet" href="css/info.css">
+
+    <!-- font HERE -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300" rel="stylesheet"><!-- font HERE -->
+
+
+    <!-- prepare loading HERE -->
+   
+<style>
+  html {
+   
+    overflow: hidden;
+  }
+  #btngo{
+  position: absolute;
+    left:45%;
+    top: 80%;
+
+
+}
+.btn-xl {
+  padding: 1rem 2rem;}
+  
+.btn {
+  
+  opacity: 1 !important;
+  font-size: 1em;
+  font-weight: 700;
+  text-transform: uppercase;
+  border: none;
+  border-radius: 300px;
+  font-family: 'Roboto Condensed', sans-serif;
+ 
+}
+  }
+</style>
+</head>
+
+<body>
+
+<!-- First Loader HERE-->
+
+<section>
+<div class="container" >
+    <div class="row text-center">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 load-sad">
+          <div class="wrapper">
+            <div id="loader-wrapper">
+            
+             <div class="preloader"style="border-style:solid; border-color: #f0f0f0; padding:2em; border-width: 5px 5px 5px 20px;">      
+                <div  class="sk-cube-grid" >
+                            <div class="sk-cube sk-cube1"></div>
+                            <div class="sk-cube sk-cube2"></div>
+                            <div class="sk-cube sk-cube3"></div>
+                            <div class="sk-cube sk-cube4"></div>
+                            <div class="sk-cube sk-cube5"></div>
+                            <div class="sk-cube sk-cube6"></div>
+                            <div class="sk-cube sk-cube7"></div>
+                            <div class="sk-cube sk-cube8"></div>
+                            <div class="sk-cube sk-cube9"></div>
+             </div>
+             <div class="loader" style="font-family: 'Raleway', sans-serif; ">
+                        <span1>{</span1>Few's Portfolio<span1>}</span1></br>
+                </div>
+                <p style ="font-family: 'Raleway', sans-serif; " class="dotload">Loading </p>
+</div>
+                    <div class="loader-section section-left"></div>
+                    <div class="loader-section section-right"></div>
+
+                </div>  
+             </div>
+
+    </div>
+</div>
+</section>
+<!--navbar-->
+
+
+
+<!--AFTER LOAD-->
+
+<section id="home" >
+
+
+  <header class="masthead d-flex" id="home" >
+      <div id='stars'></div>
+      <div id='stars2'></div>
+      <div id='stars3'></div>
+    
+    <div class="container my-auto">
+      <div class="row">
+
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <div class="graphic">
+            
+              <h2 class="text-left text-lg text-uppercase my-0">
+                <div class="has-animation animation-ltr" data-delay="100">
+                  <p class="bigger">
+                    <strong> ♥ PASSION </strong>
+                  </p>
+                </div>
+              </h2>
+              <div class="graphiccontent">
+                <div class="has-animation animation-ltr" data-delay="100">
+                  <p class="bigger" >
+                    <h>Welcome and Glad to meet U  </h><br>
+                    
+                    
+                  </p>
+                </div>
+
+              </div>
+            
+          </div>
+        </div>
+
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+            <div class="coding">
+              
+                <h2 class="text-right text-lg text-uppercase my-0">
+                  <div class="has-animation animation-ltr" data-delay="100">
+                    <p class="bigger">
+                      <strong> { DREAM } </strong>
+                    </p>
+                  </div>
+                </h2>
+                <div class="codecontent">
+                  <div class="has-animation animation-ltr" data-delay="100">
+                    <p class="bigger" >
+                      <h>What about me ?</h><br>
+                       
+                    </p>
+                   
+                  </div>
+                 
+                </div>
+              
+            </div>
+            
+          </div>
+
+    </div>
+            
+          <div class="container-fluid-btn" id="btngo">
+                   <a class="btn btn-primary btn-xl js-scroll-trigger" href="profilehi.html" >Click me carefully</a>
+               </div>     
+    </div>
+    
+    </div>
+
+    </div>
+              
+  </header>
+  
+</section>
+
+
+<div class="cd-cover-layer"></div>
+<div class="cd-loading-bar"></div>
+    <!-- GOGOGOGOGOGOGOGO-->
+
+  
+ <!-- SCRIPT HERE -->
+
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/textfade.js"></script>
+<script src="js/go.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  
+ 
+  <script src="js/index.js"></script>
+  <script src="js/info.js"></script>
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+ <!-- SCRIPT HEaRE -->
+
+</body>
+</html>
